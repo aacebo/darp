26edd4fde9d71342b38ea7ae065407c532b1e26d
Alex Acebo <aacebowork@gmail.com>
2026-03-24 12:05:53 -0700

    stuff

--- src/template/diagnostic/label.rs
+++ src/template/diagnostic/label.rs
@@ -9,8 +9,8 @@ pub struct Label {
 }
 
 impl Label {
-    pub fn new(span: Span, message: impl std::fmt::Display) -> Self {
-        Self {
+    pub fn new(span: Span, message: impl std::fmt::Display) -> build::LabelBuilder {
+        build::LabelBuilder {
             span,
             level: Level::Unknown,
             message: message.to_string(),
@@ -32,5 +32,44 @@ impl Label {
 
     pub fn suggestions(&self) -> &[String] {
         &self.suggestions
+    }
+}
+
+impl Span {
+    pub fn label(&self, message: impl std::fmt::Display) -> build::LabelBuilder {
+        Label::new(*self, message)
+    }
+}
+
+pub mod build {
+    use super::*;
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct LabelBuilder {
+        pub(super) span: Span,
+        pub(super) level: Level,
+        pub(super) message: String,
+        pub(super) suggestions: Vec<String>,
+    }
+
+    impl LabelBuilder {
+        pub fn level(mut self, level: Level) -> Self {
+            self.level = level;
+            self
+        }
+
+        pub fn suggestion(mut self, suggestion: impl std::fmt::Display) -> Self {
+            self.suggestions.push(suggestion.to_string());
+            self
+        }
+
+        pub fn build(self) -> Label {
+            Label {
+                span: self.span,
+                level: self.level,
+                message: self.message,
+                suggestions: self.suggestions,
+            }
+        }
     }
 }
--- src/template/diagnostic/mod.rs
+++ src/template/diagnostic/mod.rs
@@ -17,8 +17,8 @@ pub struct Diagnostic {
 }
 
 impl Diagnostic {
-    pub fn new(span: Span, code: Code) -> Self {
-        Self {
+    pub fn new(span: Span, code: Code) -> build::DiagnosticBuilder {
+        build::DiagnosticBuilder {
             span,
             code,
             message: None,
@@ -43,4 +43,46 @@ impl Diagnostic {
     }
 }
 
-pub mod build {}
+impl Span {
+    pub fn emit(&self, code: Code) -> build::DiagnosticBuilder {
+        Diagnostic::new(*self, code)
+    }
+}
+
+pub mod build {
+    use super::*;
+
+    #[derive(Debug, Clone)]
+    pub struct DiagnosticBuilder {
+        pub(super) span: Span,
+        pub(super) code: Code,
+        pub(super) message: Option<String>,
+        pub(super) labels: Vec<Label>,
+    }
+
+    impl DiagnosticBuilder {
+        pub fn message(mut self, message: impl std::fmt::Display) -> Self {
+            self.message = Some(message.to_string());
+            self
+        }
+
+        pub fn label(mut self, label: Label) -> Self {
+            self.labels.push(label);
+            self
+        }
+
+        pub fn labels(mut self, labels: Vec<Label>) -> Self {
+            self.labels = labels;
+            self
+        }
+
+        pub fn build(self) -> Diagnostic {
+            Diagnostic {
+                span: self.span,
+                code: self.code,
+                message: self.message,
+                labels: self.labels,
+            }
+        }
+    }
+}
